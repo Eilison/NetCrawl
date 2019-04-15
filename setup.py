dd437b81a8af64470fe088c343983729b413fef1
ELtech <1277886419@qq.com>
2019-04-15 15:51:41 +0800

    添加模块文件

---
+++ setup.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='netcrawl',
+    version='v1.0.1',
+    author='eilison',
+    author_email='1277886419@qq.com',
+    url='https://github.com/Eilison/NetCrawl.git',
+    description=u'base crawl features',
+    license='MIT License',
+    packages=find_packages(),
+    platforms=["all"],
+    install_requires=[
+        "selenium",
+        "logging"
+    ]
+)

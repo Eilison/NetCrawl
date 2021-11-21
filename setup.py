3e915da992ceb6c7db71f2114e9175462da97f94
EL <1277886419@qq.com>
2021-11-22 00:16:54 +0800

    修改支持python3的语法 去掉logging的显式导入

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='netcrawl',
-    version='v1.0.5.dev1',
+    version='v1.1.1',
     author='eilison',
     author_email='1277886419@qq.com',
     url='https://github.com/Eilison/NetCrawl.git',
-    description=u'base crawl features',
+    description=u'support ',
     license='MIT License',
     packages=find_packages(),
     platforms=["all"],
     install_requires=[
-        "selenium",
-        "logging"
+        "selenium"
     ]
 )
